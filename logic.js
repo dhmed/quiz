99022a853ca2edac5fe6b15c360a907635c2c870
Diego Medina <dhmedina88@gmail.com>
2023-12-05 16:46:55 +0000

    added let code

---
+++ logic.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    question: "What is JavaScript?",
+    options: ["A programming language computer", "A type of food", "A type of plant"],
+    correctAnswer: 0,
+  },
+  // Add more questions as needed
+];
+
+let currentQuestionIndex = 0;
+let timeRemaining = 60; // Initial time, adjust as needed
+let score = 0;
