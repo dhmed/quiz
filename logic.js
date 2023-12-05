3e79a941236f452269b0cdc8ba3bdbb53fd4579a
Diego Medina <dhmedina88@gmail.com>
2023-12-05 16:50:41 +0000

    added function

--- logic.js
+++ logic.js
@@ -10,3 +10,28 @@ const questions = [
 let currentQuestionIndex = 0;
 let timeRemaining = 60; // Initial time, adjust as needed
 let score = 0;
+
+document.getElementById('start-btn').addEventListener('click', startQuiz);
+
+function startQuiz() {
+  document.getElementById('start-screen').classList.add('hide');
+  document.getElementById('questions').classList.remove('hide');
+  displayQuestion();
+  startTimer();
+}
+
+function displayQuestion() {
+  const questionTitle = document.getElementById('question-title');
+  const optionsContainer = document.getElementById('options-container');
+  const currentQuestion = questions[currentQuestionIndex];
+
+  questionTitle.textContent = currentQuestion.question;
+  optionsContainer.innerHTML = '';
+
+  currentQuestion.options.forEach((option, index) => {
+    const button = document.createElement('button');
+    button.textContent = option;
+    button.addEventListener('click', () => checkAnswer(index));
+    optionsContainer.appendChild(button);
+  });
+}
